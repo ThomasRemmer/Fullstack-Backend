faa73a579e7aa043777f7037df04b932b56f333a
Thomas Remmer <remmert765@gmail.com>
2022-09-07 15:45:00 +0100

    set up API

---
+++ src/main/java/com/example/ImageSite/ImageSiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.ImageSite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ImageSite/ImageSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ImageSite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
